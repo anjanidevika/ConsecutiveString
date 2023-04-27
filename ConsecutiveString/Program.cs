b7a0dedcf25771b8bd469c6a60ebab39a3c40f5b
anjanidevika <adevika@kiproshamerica.com>
2023-04-27 16:28:09 +0530

    Add project files.

---
+++ ConsecutiveString/Program.cs
@@ -0,0 +1,20 @@
+﻿//program to check for consecutive strings in text
+using System;
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter the test");
+        string text = Console.ReadLine();
+        Console.WriteLine("test for same consecutive letters in text is " + test(text));
+    }
+    public static bool test(string text)
+    {
+        for (int i = 0; i < text.Length - 1; i++)
+        {
+            if (text[i] == text[i + 1])
+                return true;
+        }
+        return false;
+    }
+}
